8e967788cd830a499e2f6ccfa073bd0b7b00d34f
ankayca <ankayca2121@gmail.com>
2021-08-23 10:51:21 +0500

    uart comments added

--- src/AC_UART.c
+++ src/AC_UART.c
@@ -8,9 +8,19 @@
 
 void USART1_Transmitter(unsigned char data)
 {
+	/*********** STEPS FOLLOWED *************
 
-    while((USART1->SR & (1<<7)) == 0);
-    USART1->DR = data;
+	1. Write the data to send in the USART_DR register (this clears the TXE bit). Repeat this
+		 for each data to be transmitted in case of single buffer.
+	2. After writing the last data into the USART_DR register, wait until TC=1. This indicates
+		 that the transmission of the last frame is complete. This is required for instance when
+		 the USART is disabled or enters the Halt mode to avoid corrupting the last transmission.
+
+		****************************************/
+
+
+    while((USART1->SR & (1<<6)));	// Wait for TC to SET.. This indicates that the data has been transmitted
+    USART1->DR = data;				// load the data into DR register
 
 }
 
@@ -23,6 +33,16 @@ void AC_printstring(char *str)
 
 	}
 }
+/******* STEPS FOLLOWED ********
+
+	1. Enable the UART CLOCK and GPIO CLOCK
+	2. Configure the UART PINs for ALternate Functions
+	3. Enable the USART by writing the UE bit in USART_CR1 register to 1.
+	4. Program the M bit in USART_CR1 to define the word length.
+	5. Select the desired baud rate using the USART_BRR register.
+	6. Enable the Transmitter/Receiver by Setting the TE and RE bits in USART_CR1 Register
+
+	********************************/
 void AC_UARTInit(void)
 {
 	GPIO_InitTypeDef gpio_init;
